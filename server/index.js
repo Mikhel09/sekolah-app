77bf56c0822dd4257854015f5d95a8195e4c0ca3
Mikhel09 <66584105+Mikhel09@users.noreply.github.com>
2026-07-16 07:04:01 +0700

    Fix CORS - tambah https:// di origin

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'sekolah-app-eta.vercel.app'],
+  origin: ['http://localhost:5173', 'https://sekolah-app-eta.vercel.app'],
 }));
 app.use(express.json());
 
